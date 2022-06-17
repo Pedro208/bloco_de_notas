28f657472de71121812e40f90616263694f8e4be
Pedro Augusto <pedrodaluzmota208@gmail.com>
2022-06-16 23:54:28 -0300

    notes

--- net_here/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:net_here/helpers/geolocation.dart';
-import 'screns/home_page.dart';
-
-void main() {
-  print("_______________teste______________");
-  print(Geolocation().determinePosition());
-  runApp(const MyApp());
-}
